4937df122f3d0d43278bd2064a336a760cc93e6c
kuncarous <mscsirmaster@gmail.com>
2024-04-28 13:45:04 +0200

    fixed import order

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,8 @@
+import './loadenv';
+
 import * as gRPC from '~/grpc';
 import { logger } from '~/logger';
 import app from '~/server';
-import './loadenv';
 
 const port = Number(process.env.API_PORT || 8701);
 async function start() {
